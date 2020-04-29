d20bae0a7b91cbcb39b61b29a594073b11cfe504
Nadiia Heckman <nkysilheckman@getty.edu>
2020-04-29 15:28:15 -0700

    DEV-4909, added new menu item to go to OpenText Admin page

--- cs_gt_go_help/GettyGoView.js
+++ cs_gt_go_help/GettyGoView.js
@@ -1,14 +1,21 @@
 (function(exports)
 {
-	function setupGoGetty(event, resource)
-	{
+	function setupGoGetty(event, resource){
 		return true;
-	};;
+	};
+	
+	function setupGoAdmin(){
+		return true;
+	}
 
-	function goGetty(event, resource)
-	{
+	function goGetty(event, resource){
 		window.open("http://go.getty.edu/GO/cmsGO?page=1353", "PopupWindow", "width=600,height=600,scrollbars=yes,resizable=no");
 	};
+	
+	function goAdmin(event, resource){
+		window.open("/teams", "PopupWindow", "width=600,height=600,scrollbars=yes,resizable=no");
+	}
 
 	otui.HelpActions.register({'name' : 'gogetty', 'text' : otui.tr('GO Help Page'), 'setup' : setupGoGetty, 'select' : goGetty}, 2);
+	otui.HelpActions.register({'name' : 'goadmin', 'text' : otui.tr('OpenText Admin'), 'setup' : setupGoAdmin, 'select' : goAdmin}, 3);
 })(window);
