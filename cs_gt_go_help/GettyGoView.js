e2d0284d085de5eb567654f314d5c4ec264b4c7a
Nadiia Heckman <nkysilheckman@getty.edu>
2020-05-04 23:17:25 -0700

    DEV-4909, swap admin and help menu entries

--- cs_gt_go_help/GettyGoView.js
+++ cs_gt_go_help/GettyGoView.js
@@ -16,6 +16,7 @@
 		window.open("/teams", "targetWindow", "width=1024,height=768,scrollbars=yes,resizable=yes");
 	}
 
-	otui.HelpActions.register({'name' : 'gogetty', 'text' : otui.tr('GO Help Page'), 'setup' : setupGoGetty, 'select' : goGetty}, 2);
-	otui.HelpActions.register({'name' : 'goadmin', 'text' : otui.tr('Administration Portal'), 'setup' : setupGoAdmin, 'select' : goAdmin}, 3);
+	otui.HelpActions.register({'name' : 'goadmin', 'text' : otui.tr('Administration Portal'), 'setup' : setupGoAdmin, 'select' : goAdmin}, 2);
+	otui.HelpActions.register({'name' : 'gogetty', 'text' : otui.tr('GO Help Page'), 'setup' : setupGoGetty, 'select' : goGetty}, 3);
+	
 })(window);
